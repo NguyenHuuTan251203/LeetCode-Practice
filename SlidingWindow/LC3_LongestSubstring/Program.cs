10ca6c2546bca3d27204c90573f3b04942dbcf03
NguyenHuuTan <tan25122003@gmail.com>
2025-08-18 12:32:38 +0700

    SlidingWindow

---
+++ SlidingWindow/LC3_LongestSubstring/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_leetcode
+{
+    internal class Program
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int left = 0, lengthOfLongestSubstring = 0;
+            HashSet<char> set = new HashSet<char>();
+            for (int right = 0; right < s.Length; right++)
+            {
+                while (set.Contains(s[right]))
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+                set.Add(s[right]);
+                lengthOfLongestSubstring = Math.Max(lengthOfLongestSubstring, right - left + 1);
+            }
+            return lengthOfLongestSubstring;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "pwwkew";
+            int lengh_array_longest = LengthOfLongestSubstring(s);
+            Console.WriteLine(lengh_array_longest);
+        }
+    }
+}
---
+++ SlidingWindow/LC643/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _643_
+{
+    internal class Program
+    {
+
+        //Given an integer array nums and an integer k, return the maximum average value of a subarray of length k.
+        //Any subarray of length k is a contiguous subarray of length k that can be found in the array.
+        //The average value of a subarray is the sum of the elements in the subarray divided by k.
+        //Example 1:
+        //Input: nums = [1,12,-5,-6,50,3], k = 4
+        //Output: 12.75
+        //Thanks you copilot for the help
+
+
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            int sum = 0;
+            for(int i = 0; i < k;i++)
+            {
+                sum += nums[i];
+            }
+            int maxSum=sum;
+
+            for(int i  = k;i < nums.Length;i++)
+            {
+                sum = sum - nums[i - k] + nums[i];
+                maxSum = Math.Max(maxSum, sum);
+            }
+            return (double) maxSum/k;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 5};
+            int k = 1;
+            double maxava = FindMaxAverage(nums, k);
+            Console.WriteLine(maxava);
+        }
+    }
+}
