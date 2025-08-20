90c5ed67eb84df3418da9a2b001e9536d365eb9e
NguyenHuuTan <tan25122003@gmail.com>
2025-08-20 21:36:18 +0700

    Add to a solution today

---
+++ Array/Easy/Leet_217/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _217_leetcode
+{
+    internal class Program
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            int length = nums.Length;
+            if (length == 1) return false;
+            HashSet<int> set = new HashSet<int>(length);
+            for (int i = 0; i < length; i++)
+                if (!set.Add(nums[i]))
+                    return true;
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            bool rs = ContainsDuplicate(nums);
+            Console.WriteLine(rs);
+        }
+    }
+}
---
+++ TwoPointer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_
+{
+    internal class Program
+    {
+        public static int MaxArea(int[] height)
+        {
+            int left = 0,right = height.Length - 1,maxArea = 0;
+            while (left < right)
+            {
+                int area = (right - left) * Math.Min(height[left], height[right]);
+                maxArea = Math.Max(maxArea, area);
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+
+            }
+            return maxArea;
+        }
+        static void Main(string[] args)
+        {
+            int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Console.WriteLine(MaxArea(height));
+        }
+    }
+}
