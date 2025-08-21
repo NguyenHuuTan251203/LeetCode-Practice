e7822bc7e3c9c8ff5b3e8087243b426e50bb0250
NguyenHuuTan <tan25122003@gmail.com>
2025-08-21 23:37:14 +0700

    some solutions was written days ago , i push to manage folder easiest

---
+++ SlidingWindow/Medium/LC1004/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1004_leetcode
+{
+    internal class Program
+    {
+        public static int LongestOnes(int[] nums, int k)
+        {
+            int zero = 0, left = 0,maxCount = 0;
+            for(int right = 0; right < nums.Length;right++)
+            {
+                if (nums[right] == 0)
+                    zero++;
+                while (zero > k )
+                {
+                    if (nums[left] == 0) zero--;
+                    left++;
+                }    
+                maxCount = Math.Max(maxCount, right - left +1);
+            }
+
+
+            return maxCount;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 }; int k = 3;
+            int countMax = LongestOnes(nums, k);
+            Console.WriteLine(countMax);
+        }
+    }
+}
+
