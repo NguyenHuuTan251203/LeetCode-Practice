c4f2137f6fc817003438c8d23abdef04c16fe51b
NguyenHuuTan <tan25122003@gmail.com>
2025-08-21 23:17:19 +0700

    Create Program.cs

---
+++ Array/Easy/Leet_14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_leetcode
+{
+    internal class Program
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            int length = strs.Length;
+            if (length == 0) return "";
+            StringBuilder rs = new StringBuilder();
+            strs = strs.OrderBy(x => x.Length).ToArray();
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for(int j = 1; j < length;j++)
+                {
+                    if (strs[0][i] != strs[j][i])
+                    {
+                        return rs.ToString();
+                    }
+                }
+                rs.Append(strs[0][i]);
+            }
+            return rs.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string[] strs = {"reflower", "flow", "flight" };
+            string result = LongestCommonPrefix(strs);
+            Console.WriteLine(result);
+        }
+    }
+}
