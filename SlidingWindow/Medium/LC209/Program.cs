ad8f3217d1291225659435559f9bfb143cff5d53
NguyenHuuTan <tan25122003@gmail.com>
2025-08-23 00:00:34 +0700

    update today one solutions

---
+++ SlidingWindow/Medium/LC209/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _209_leetcode
+{
+    internal class Program
+    {
+        public static int MinSubArrayLen(int target, int[] nums)
+        {
+            int length = nums.Length, left = 0, sum = 0, minimalLength = int.MaxValue;
+
+            for (int right = 0; right < length; right++)
+            {
+                sum += nums[right];
+                while (sum >= target)
+                {
+                    minimalLength = Math.Min(minimalLength, right - left + 1);
+                        sum -= nums[left];
+                    left++;
+                }
+            }
+            return minimalLength == int.MaxValue ? 0 : minimalLength;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int target = 11;
+            Console.WriteLine(MinSubArrayLen(target, nums));
+        }
+    }
+}
