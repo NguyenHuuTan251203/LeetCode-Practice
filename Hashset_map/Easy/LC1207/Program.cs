5fe967ea42d93f8e95f8c2328433b3ebc6682cbb
NguyenHuuTan <tan25122003@gmail.com>
2025-09-09 13:00:48 +0700

    Leetcode-daily-easy-level

---
+++ Hashset_map/Easy/LC1207/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1207_leetcode
+{
+    internal class Program
+    {
+        public static bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            foreach (int x in arr)
+            {
+                if (!result.ContainsKey(x))
+                    result.Add(x, 1);
+                else result[x]++;
+            }
+            HashSet<int> set = new HashSet<int>();
+            foreach (var x in result)
+                if (!set.Add(x.Value))
+                    return false;
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 1, 3 };
+            var rs = UniqueOccurrences(arr);
+            Console.WriteLine(rs);
+        }
+    }
+}
---
+++ Hashset_map/Easy/LC2215/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2215_leetcode
+{
+    internal class Program
+    {
+        public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            
+            HashSet<int> arr1 = new HashSet<int>(nums1);
+            HashSet<int> arr2 = new HashSet<int>(nums2);
+
+            var only1 = new HashSet<int>(arr1);
+            var only2 = new HashSet<int>(arr2);
+
+            only1.ExceptWith(only2);
+            only2.ExceptWith(only1);    
+
+
+            return new List<IList<int>> { only1.ToList() ,only2.ToList()};
+        }
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3,3 }, nums2 = { 1,1,2,2 };
+            IList<IList<int>> rs = FindDifference(nums1, nums2);
+            //foreach (var item in rs)
+            //{
+
+            //}
+        }
+    }
+}
